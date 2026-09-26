c247c69abf1e8bbf732f096ca627c0435b5ba380
Aditya Suryawanshi <adityasuryawanshi1704@gmail.com>
2026-09-27 02:26:00 +0530

    Time: 40 ms (47.66%), Space: 86.4 MB (98.99%) - LeetHub

---
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]!=nums[i+1])  ans+=(i+1);
+        }
+        return ans;
+    }
+};
