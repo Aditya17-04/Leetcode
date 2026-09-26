0046ebdece1d7e212d1e94f6f548b2c0237a96ad
Aditya Suryawanshi <adityasuryawanshi1704@gmail.com>
2026-09-26 17:11:31 +0530

    Time: 0 ms (100%), Space: 22.9 MB (97.94%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,15 +14,13 @@ public:
     int height(TreeNode* root){
         if(root == NULL) return 0;
         int left = height(root->left);
+        if(left == -1)  return -1;
         int right = height(root->right);
+        if(right == -1) return -1;
+        if(abs(left-right)>1)   return -1;
         return max(left,right)+1;
     }
     bool isBalanced(TreeNode* root) {
-        if(root == NULL)    return true;
-        int left = height(root->left);
-        int right = height(root->right);
-        int diff = abs(left-right);
-        if(isBalanced(root->left) && isBalanced(root->right) && diff<=1)   return true;
-        return false;
+        return height(root) != -1;
     }
 };
